7f9153a7628d1ada1eefbb7a3648b5a9ad3a8003
Zachary Lynn <zalynn25@hotmail.com>
2020-11-15 18:52:41 -0500

    seeds

---
+++ seed.sql
@@ -0,0 +1,24 @@
+use staff;
+
+INSERT INTO department
+  (name)
+VALUES
+  ("Production"),
+  ("Research and Developement"),
+  ("Administrative"),
+  ("Human Resources");
+
+INSERT INTO role
+  (title, salary, department_id),
+
+VALUES
+  ("Technician", 70000, 1);
+  ("Engineer", 95000, 2);
+  ("CEO", 250000, 3);
+  ("HR Director", 199000, 4);
+
+INSERT INTO employee 
+  (first_name, last_name, role_id, manager_id)
+
+VALUES   
+  ("Zach, Lynn", 1, 1) 
