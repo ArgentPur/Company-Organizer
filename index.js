ffd56b01ad74f956133dccafd2577f06079d4da1
Zachary Lynn <zalynn25@hotmail.com>
2020-11-16 22:36:36 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const {prompt} = require("inquirer");
 const logo = require("asciiart-logo");
 const db = require("./db");
-require("");
+const consoleT = require("console.table");
--- schema.sql
+++ schema.sql
@@ -5,13 +5,14 @@ USE staff;
 CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
-    
+
 );
 
 CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) UNIQUE NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
     Index dep_ind (department_id),
     CONSTRAINT fy_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 
@@ -26,9 +27,6 @@ CREATE TABLE employee (
     CONSTRAINT fy_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT UNSIGNED,
     Index man_ind (man_id),
-    CONSTRAINT fy_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT UNSIGNED,
-    INDEX man_ind(manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 
 );
