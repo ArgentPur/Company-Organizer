d2dce45a362aff578bf754ff61053ee90ab4b9ca
Zachary Lynn <zalynn25@hotmail.com>
2020-11-17 20:38:55 -0500

    update

--- db/index_db.js
+++ db/index_db.js
@@ -8,5 +8,13 @@ class DB {
         return this.connection.query(
             "SELECT employee.id, employee.first_Name, employee.last_Name, role.title, department.name, AS department, role.salary, CONCAT(manager.first_Name, ' ', manager.last_Name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
-    }
+    };
+    findAllRoles() {
+        return this.connection.query(
+            `SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;`
+        )
+    };
+
 }
+
+module.exports = new DB(connection);
